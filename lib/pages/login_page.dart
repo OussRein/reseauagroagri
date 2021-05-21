4a8befaf87a33711cc7aa3c2f37d68d01ee7c5f7
OussRein <ous.1438@gmail.com>
2021-05-21 18:01:27 +0100

    adding forgotten password

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,9 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth.dart';
-import '../models/http_exception.dart';
 
-enum AuthMode { Signup, Login }
+enum AuthMode { LOGIN, SIGNUP, FORGOTPASSWORD }
 
 class AuthPage extends StatelessWidget {
   static const String ROUTE = '/auth';
@@ -97,7 +96,7 @@ class AuthCard extends StatefulWidget {
 
 class _AuthCardState extends State<AuthCard> {
   final GlobalKey<FormState> _formKey = GlobalKey();
-  AuthMode _authMode = AuthMode.Login;
+  AuthMode _authMode = AuthMode.LOGIN;
   Map<String, String> _authData = {
     'email': '',
     'password': '',
@@ -133,15 +132,15 @@ class _AuthCardState extends State<AuthCard> {
       _isLoading = true;
     });
     try {
-      if (_authMode == AuthMode.Login) {
+      if (_authMode == AuthMode.LOGIN) {
         await Provider.of<Auth>(context, listen: false)
             .login(_authData['email'], _authData['password']);
       } else {
         await Provider.of<Auth>(context, listen: false)
             .signup(_authData['email'], _authData['password']);
       }
-    } on HttpException catch (error) {
-      var errorMessage = "Authentication failed";
+    } catch (error) {
+      var errorMessage = "Authentication failed - ";
       if (error.toString().contains('EMAIL_EXISTS')) {
         errorMessage = "Email already exists";
       } else if (error.toString().contains('INVALID_EMAIL')) {
@@ -152,12 +151,11 @@ class _AuthCardState extends State<AuthCard> {
         errorMessage = "Email doesn't exist";
       } else if (error.toString().contains('INVALID_PASSWORD')) {
         errorMessage = "Invalid password";
+      } else {
+        errorMessage =
+            "Couldn't authenticate you, verify network and try again";
       }
 
-      _showErrorDialog(errorMessage);
-    } catch (error) {
-      var errorMessage =
-          "Couldn't authenticate you, verify network and try again";
       _showErrorDialog(errorMessage);
     }
     setState(() {
@@ -165,16 +163,37 @@ class _AuthCardState extends State<AuthCard> {
     });
   }
 
-  void _switchAuthMode() {
-    if (_authMode == AuthMode.Login) {
+  /*void _switchAuthMode() {
+    if (_authMode == AuthMode.LOGIN) {
       setState(() {
-        _authMode = AuthMode.Signup;
+        _authMode = AuthMode.SIGNUP;
       });
     } else {
       setState(() {
-        _authMode = AuthMode.Login;
+        _authMode = AuthMode.LOGIN;
       });
     }
+  }*/
+
+  void _changeFormToSignUp() {
+    _formKey.currentState.reset();
+    setState(() {
+      _authMode = AuthMode.SIGNUP;
+    });
+  }
+
+  void _changeFormToLogin() {
+    _formKey.currentState.reset();
+    setState(() {
+      _authMode = AuthMode.LOGIN;
+    });
+  }
+
+  void _changeFormToPasswordReset() {
+    _formKey.currentState.reset();
+    setState(() {
+      _authMode = AuthMode.FORGOTPASSWORD;
+    });
   }
 
   @override
@@ -186,9 +205,8 @@ class _AuthCardState extends State<AuthCard> {
       ),
       elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        height: 400,
+        constraints: BoxConstraints(minHeight: 360),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
         child: Form(
@@ -196,7 +214,8 @@ class _AuthCardState extends State<AuthCard> {
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                TextFormField(
+                _showEmailInput(),
+                /*TextFormField(
                   decoration: InputDecoration(labelText: 'E-Mail'),
                   keyboardType: TextInputType.emailAddress,
                   validator: (value) {
@@ -208,8 +227,9 @@ class _AuthCardState extends State<AuthCard> {
                   onSaved: (value) {
                     _authData['email'] = value;
                   },
-                ),
-                TextFormField(
+                ),*/
+                _showPasswordInput(),
+                /*TextFormField(
                   decoration: InputDecoration(labelText: 'Password'),
                   obscureText: true,
                   controller: _passwordController,
@@ -223,12 +243,12 @@ class _AuthCardState extends State<AuthCard> {
                     _authData['password'] = value;
                   },
                 ),
-                if (_authMode == AuthMode.Signup)
+                if (_authMode == AuthMode.SIGNUP)
                   TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
+                    enabled: _authMode == AuthMode.SIGNUP,
                     decoration: InputDecoration(labelText: 'Confirm Password'),
                     obscureText: true,
-                    validator: _authMode == AuthMode.Signup
+                    validator: _authMode == AuthMode.SIGNUP
                         ? (value) {
                             if (value != _passwordController.text) {
                               return 'Passwords do not match!';
@@ -239,13 +259,12 @@ class _AuthCardState extends State<AuthCard> {
                   ),
                 SizedBox(
                   height: 20,
-                ),
-                if (_isLoading)
-                  CircularProgressIndicator()
-                else
+                ),*/
+                if (_isLoading) CircularProgressIndicator(),
+                /*else
                   ElevatedButton(
                     child:
-                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+                        Text(_authMode == AuthMode.LOGIN ? 'LOGIN' : 'SIGN UP'),
                     onPressed: _submit,
                     style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.all(
@@ -257,10 +276,13 @@ class _AuthCardState extends State<AuthCard> {
                       )),
                       //padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
                     ),
-                  ),
-                TextButton(
+                  ),*/
+                _showPrimaryButton(),
+                _showSecondaryButton(),
+                _showForgotPasswordButton(),
+                /*TextButton(
                   child: Text(
-                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+                      "${_authMode == AuthMode.LOGIN ? "S'inscrire" : "Se connecter"}"),
                   onPressed: _switchAuthMode,
                   //padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
                   style: ButtonStyle(
@@ -273,12 +295,170 @@ class _AuthCardState extends State<AuthCard> {
                     tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                     //padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
                   ),
-                ),
+                ),*/
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _showEmailInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+      child: new TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Email',
+            icon: new Icon(
+              Icons.mail,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) => _authData['email'] = value.trim(),
+      ),
+    );
+  }
+
+  Widget _showPasswordInput() {
+    if (_authMode != AuthMode.FORGOTPASSWORD) {
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+        child: new TextFormField(
+          maxLines: 1,
+          obscureText: true,
+          autofocus: false,
+          decoration: new InputDecoration(
+              hintText: 'Password',
+              icon: new Icon(
+                Icons.lock,
+                color: Colors.grey,
+              )),
+          validator: (value) =>
+              value.isEmpty ? 'Le mot de passe ne peut pas etre vide' : null,
+          onSaved: (value) => _authData['password'] = value.trim(),
+        ),
+      );
+    } else {
+      return new Text('Un mail vas etre envoyer a votre boite!');
+    }
+  }
+
+  Widget _showPrimaryButton() {
+    return new Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 0.0),
+        child: SizedBox(
+          height: 40.0,
+          child: new ElevatedButton(
+            style: ButtonStyle(
+              elevation: MaterialStateProperty.resolveWith<double>(
+                  (Set<MaterialState> states) {
+                if (states.contains(MaterialState.pressed)) return 16;
+                return null;
+              }),
+              foregroundColor: MaterialStateProperty.all(
+                Theme.of(context).primaryColor,
+              ),
+              shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              )),
+            ),
+            child: _textPrimaryButton(),
+            onPressed: _submit,
+          ),
+        ));
+  }
+
+  Widget _showSecondaryButton() {
+    return new TextButton(
+      child: _textSecondaryButton(),
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all(
+          Theme.of(context).primaryColor,
+        ),
+        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        )),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      ),
+      onPressed: _authMode == AuthMode.LOGIN
+          ? _changeFormToSignUp
+          : _changeFormToLogin,
+    );
+  }
+
+  Widget _showForgotPasswordButton() {
+    return new TextButton(
+      child: _authMode == AuthMode.LOGIN
+          ? new Text(
+              'Mot de passe oublié?',
+              style: new TextStyle(fontSize: 15.0, fontWeight: FontWeight.w300),
+            )
+          : new Text(
+              '',
+              style: new TextStyle(fontSize: 15.0, fontWeight: FontWeight.w300),
+            ),
+      onPressed: _changeFormToPasswordReset,
+    );
+  }
+
+  Widget _textPrimaryButton() {
+    switch (_authMode) {
+      case AuthMode.LOGIN:
+        return new Text(
+          'Login',
+          style: GoogleFonts.lato(
+            textStyle: Theme.of(context).textTheme.headline4,
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        );
+        break;
+      case AuthMode.SIGNUP:
+        return new Text(
+          'Create account',
+          style: GoogleFonts.lato(
+            textStyle: Theme.of(context).textTheme.headline4,
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        );
+        break;
+      case AuthMode.FORGOTPASSWORD:
+        return new Text(
+          'Reset password',
+          style: GoogleFonts.lato(
+            textStyle: Theme.of(context).textTheme.headline4,
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        );
+        break;
+    }
+    return new Spacer();
+  }
+
+  Widget _textSecondaryButton() {
+    switch (_authMode) {
+      case AuthMode.LOGIN:
+        return new Text('Créer un compte!');
+        break;
+      case AuthMode.SIGNUP:
+        return new Text('Vous avez deja un compte? Connectez-vous');
+        break;
+      case AuthMode.FORGOTPASSWORD:
+        return new Text('Cancel');
+        break;
+    }
+    return new Spacer();
   }
 }
